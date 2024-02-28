c204dd5d8f1521c3a1de297bffd5632ecaa203a8
Gao <gao1030953886@vip.qq.com>
2024-02-28 23:21:53 +0800

    We save the time of each station in rows and columns to the hard drive

--- Workspace/src/main.cpp
+++ Workspace/src/main.cpp
@@ -1,29 +1,8 @@
-// Copyright (C) 2013 FARO Technologies Inc.
-//
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to 
-// deal in the Software without restriction, including without limitation the 
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-// sell copies of the Software, and to permit persons to whom the Software is 
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in 
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
-// IN THE SOFTWARE.
 
 #include <windows.h>
 
 #include <core_api/lsproject.h>
 #include <core_api/lslocalworkspace.h>
-
 #include <standalone_api/lsstandalonecontext.h>
 
 #include <iostream>
@@ -31,11 +10,11 @@
 
 using namespace SCENE_API;
 
-void checkTime(ref_ptr<LSScan>);
+static void checkTime(ref_ptr<LSScan>);
 
 /// @brief	Helper function which displays a file open dialog to select a 
 ///			project or local workspace.
-LSString selectProjectOrWorkspace()
+static LSString selectProjectOrWorkspace()
 {
 	wchar_t fileName[MAX_PATH];
 
@@ -63,8 +42,7 @@ LSString selectProjectOrWorkspace()
 int wWinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPTSTR    lpCmdLine,
-	int       nCmdShow)
-{
+	int       nCmdShow){
 	// Standalone Apps always need to make sure to instantiate a standalone
 	// context before using any other API functionality. This context has to
 	// exist at least as along as any other API object!
@@ -96,62 +74,42 @@ int wWinMain(HINSTANCE hInstance,
 
 		// Acquire the number of scans contained in the workspace and display
 		// the result to the user
-		std::vector<ref_ptr<LSScan> > scans = project->getScans().get();
+		std::vector<ref_ptr<LSScan>> scans = project->getScans().get();
 		std::wstringstream msg;
 		msg << L"Project successfully loaded! The project contains " << scans.size() << L" scans.";
 		MessageBox(NULL, msg.str().c_str(), L"Success", MB_OK | MB_ICONINFORMATION);
-		::checkTime(scans[0]);
-		/*for (ref_ptr<LSScan>& scan: scans)
+		//::checkTime(scans[0]);
+		std::string dirFile = std::string("./data/output/");
+		int counterScan = 1;
+		for (ref_ptr<LSScan>& scan: scans)
 		{
-			LSString outpath(L"C:/Users/郭组/Desktop/测试数据/output/");
 			scan->loadData();
 			if (scan->dataLoaded()) {
-				uint64_t t = 0;
+				uint64_t timeOfScanPoint = 0;
 				uint64_t t1 = 0;
 				uint64_t timeCol = 0;
 				std::wstringstream msgTime;
 				std::wstringstream sizeStream;
 				int rows = scan->getNumRows();
 				int cols = scan->getNumCols();
-				outpath += L"11112.xyz";
-				scan->exportData(outpath, LSScan::XYZ);
-				std::ofstream outFile("C:/Users/郭组/Desktop/测试数据/output/time546.csv");
+				
+				std::ofstream outFile(dirFile + std::to_string(counterScan) + ".csv");
 				for (int row = 0; row < rows; ++row){
 					for (int col = 0; col < cols; ++col) {
-						scan->getTimeOfScanPoint(row, col, t);
-						outFile << t << ",";
+						scan->getTimeOfScanPoint(row, col, timeOfScanPoint);
+						outFile << timeOfScanPoint << ",";
 					}
 					outFile << "\n";
 				}
 				outFile.close();
+				counterScan++;
 			}
 			else
 			{
 				msg << L"I'm not OK ";
-				MessageBox(NULL, msg.str().c_str(), L"Success", MB_OK | MB_ICONINFORMATION);
+				MessageBox(NULL, msg.str().c_str(), L"Error", MB_OK | MB_ICONINFORMATION);
 			}
-
-		}*/
-		/*for (size_t i = 0; i < scans.size(); ++i)
-		{
-
-			if (scans[i]->dataLoaded()) {
-				msg << L"I'm OK ";
-				MessageBox(NULL, msg.str().c_str(), L"Success", MB_OK | MB_ICONINFORMATION);
-			}
-			else
-			{
-				msg << L"I'm not OK ";
-				MessageBox(NULL, msg.str().c_str(), L"Success", MB_OK | MB_ICONINFORMATION);
-			}
-		}*/
-
-		//uint64_t t;
-		//t = 0;
-		//scans[0]->getTimeOfScanPoint(0, 0, t);
-		//std::cout << t << std::endl;
-
-
+		}
 	}
 	else
 	{
@@ -181,7 +139,7 @@ int wWinMain(HINSTANCE hInstance,
 	return 0;
 }
 
-void checkTime(ref_ptr<LSScan> scan) {
+static void checkTime(ref_ptr<LSScan> scan) {
 
 	scan->loadData();
 
